08ded6e5270566a78c819c7b1b2b7f81454b9c36
unknown <samuelraposo321@gmail.com>
2026-04-03 22:59:08 -0300

    treinando a IA para aprender a eficiencia de cada treino

--- inicio.py
+++ inicio.py
@@ -1,2 +1,7 @@
 import pandas as pd
 import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+
+# Criando o "cérebro"
+modelo_ia = RandomForestRegressor(n_estimators=100)
